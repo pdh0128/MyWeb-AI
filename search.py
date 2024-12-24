4b10d072850ab2fc7d0526e36f512615b5f96fa2
pdh0128 <pdh0128a@gmail.com>
2024-12-24 11:32:02 +0900

    최적화된 검색을 위해 요약하여 내용을 요약하여 벡터DB에 저장

--- search.py
+++ search.py
@@ -2,9 +2,12 @@ from itertools import count
 
 from langchain_pinecone import PineconeVectorStore
 from langchain_openai import OpenAIEmbeddings
+from langchain_core.output_parsers import StrOutputParser
 import os
 from pinecone import Pinecone
 from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from langchain.prompts import PromptTemplate
 
 load_dotenv()
 
@@ -13,6 +16,12 @@ index_name = os.getenv("PINECONE_INDEX")
 pc = Pinecone(api_key=api_key)
 
 embeddings = OpenAIEmbeddings()
+llm = ChatOpenAI(model="gpt-4o-mini", temperature=0, openai_api_key=os.getenv("OPENAI_API_KEY"))
+temp = """다음 블로그 글을 요약해줘. 마크다운 포맷은 모두 제거하고, 내용을 간결하게 정리해줘. 요약은 원본 내용의 약 50% 정도로 축약해줘.\n내용 : {content}"""
+
+prompt = PromptTemplate(template=temp, input_variables=['content'])
+
+chain = prompt | llm | StrOutputParser()
 
 pinecone_index = pc.Index(index_name)
 
@@ -20,8 +29,11 @@ vector_store = PineconeVectorStore(index_name=index_name, embedding=embeddings)
 
 def vec_store(postid, title, content):
     text = f"Title: {title}, Content: {content}"
+    print(text)
+    summary = chain.invoke({"content": text})
+    print("요약 : " + summary)
     vector_store.add_texts(
-        texts=[text],
+        texts=[summary],
         metadatas=[{"postid": int(postid)}],
         ids=[str(postid)]
     )
@@ -40,10 +52,12 @@ def vec_delete(postid):
     pinecone_index.delete(ids=[str(postid)])
 
 def vec_update(postid, title, content):
-    pinecone_index.delete(ids=[str(postid)])
-    text = f"Title: {title}, Content: {content}"
-    vector_store.add_texts(
-        texts=[text],
+     pinecone_index.delete(ids=[str(postid)])
+     text = f"Title: {title}, Content: {content}"
+     summary =  chain.invoke({"content": text})
+     print("요약 : " + summary)
+     vector_store.add_texts(
+        texts=[summary],
         metadatas=[{"postid": int(postid)}],
         ids=[str(postid)]
     )
